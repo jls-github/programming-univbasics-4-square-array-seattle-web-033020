e64b3899d9000e050960c69949d8aecd74a57e35
John Souza <souzajohnlee@gmail.com>
2020-01-26 00:36:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   index = 0
   new_array = []
   while index != array.length + 1 do
-    new_array.push(array[index] ** 2)
+    new_array.push(array[index] * array[index])
     index += 1
   end
   return new_array
