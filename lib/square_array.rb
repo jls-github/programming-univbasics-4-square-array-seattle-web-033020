24d42b42c874cd1f73e0c16924923764d0bed307
John Souza <souzajohnlee@gmail.com>
2020-01-26 00:37:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   index = 0
   new_array = []
-  while index != array.length + 1 do
+  while index < array.length + 1 do
     new_array.push(array[index] * array[index])
     index += 1
   end
